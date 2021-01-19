bc0ee02ddb6b7f37874fa653ce3af57749fb740b
Elias Nepomuceno <eliasnepo12@gmail.com>
2021-01-18 22:57:19 -0300

    Dynamic progress bar

--- script.js
+++ script.js
@@ -11,18 +11,31 @@ const fullscreenBtn = document.querySelector('.fullscreen');
 
 // Play & Pause ----------------------------------- //
 
+function showPlayIcon() {
+    playBtn.classList.replace('fa-pause', 'fa-play');
+        playBtn.setAttribute('title', 'Play');
+}
+
 function togglePlay() {
     if (video.paused) {
         video.play();
+        playBtn.classList.replace('fa-play', 'fa-pause');
+        playBtn.setAttribute('title', 'Pause');
     } else {
         video.pause();
+        showPlayIcon();
     }
 }
 
+// On Video End, show play button icon
+video.addEventListener('ended', showPlayIcon);
+
 // Progress Bar ---------------------------------- //
 
-
-
+// Update progress bar as video play
+function updateProgress() {
+    progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
+}
 // Volume Controls --------------------------- //
 
 
@@ -37,3 +50,5 @@ function togglePlay() {
 // Event Listeners
 playBtn.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
+video.addEventListener('timeupdate', updateProgress);
+video.addEventListener('canplay', updateProgress);
